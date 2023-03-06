4a3957625f377b6cdae5f73b55beee3b862f9696
Aurelien Labrosse <aurelienl@sogilis.com>
2023-03-06 14:55:32 +0100

    feat: Use Python unittest

---
+++ jolly_jumper/python/check_jolly_test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+import unittest
+
+class JollyJumperTest(unittest.TestCase):
+
+  def test_test_harness(self):
+    self.assertEqual(True, True, "Ensure test harness is functional")
+
+
+if __name__== "__main__":
+ unittest.main()
